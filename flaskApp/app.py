e81317f8799ff736a4e5923ba291eb1175bfe186
Fdeolive <fdeolive@uvm.edu>
2024-10-09 15:21:36 -0400

    adding bootstrap

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def home():
