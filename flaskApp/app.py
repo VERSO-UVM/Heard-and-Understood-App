01ea2957b2d93edc2b74526e6208aa29c9e2b4fd
Fdeolive <fdeolive@uvm.edu>
2024-10-24 05:28:45 -0400

    working on viewAllProjects page

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from database import db,create_database
+from database import db,create_database,User,Projects,Involvement
 import sqlite3 
 
 
@@ -14,11 +14,10 @@ bootstrap = Bootstrap(app)
 engine = create_engine("sqlite:///CS2990_Final_Project.db")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HeardAndUnderstood.db'
 db.init_app(app)
+
+
 with app.app_context():
     create_database(app)
-db=SQLAlchemy(app)
-
-
 
 
 @app.route("/")
@@ -37,6 +36,68 @@ def login():
 def homepage():
     return render_template('home.html')
 
+#-----------------PI View-----------------------#
+@app.route("/viewAllProjects")
+def viewAllProjects():
+    testData()
+    all_project=Projects.query.all()
+
+    return render_template('viewAllProjects.html',all_project=all_project)
+
+
+@app.route("/viewAllUsers")
+def viewAllUsers():
+
+    return render_template()
+
+
+@app.route("/generateNewProjectCode")
+def generateNewProjectCode():
+
+    return render_template()
+
+
+@app.route("/groudTruthUpdates")
+def groudTruthUpdates():
+
+    return render_template()
+
+def testData():
+    
+#     db.session.add_all([
+#     User(email='JaneDoe1@example.com', username='JaneDoe', password='password1', status=1),
+#     User(email='JoeDoe@example.com', username='JohnDoe', password='password2', status=0),
+   
+#    ]
+# )
+#     db.session.commit()
+
+#     # Create Projects
+#     db.session.add_all([
+#     Projects(projectName="Project1",projectCode='1234', piCreator='JaneDoe1@example.com'),
+#     Projects(projectName="Project2",projectCode='4321', piCreator='JaneDoe1@example.com')]
+#     )
+
+   
+#     db.session.commit()  
+
+    # # # Create Involvements
+    # db.session.add_all([
+    #     Involvement(id=1,user_id="JaneDoe1@example.com", project_id="Project1"),
+    #     Involvement(id=2,user_id="JaneDoe1@example.com", project_id="Project1"),
+    #     Involvement(id=3,user_id="JaneDoe1@example.com", project_id="Project2")
+    # ])
+    
+   
+   
+    db.session.commit()  
+
+   
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001, threaded=False)
+
--- flaskApp/database.py
+++ flaskApp/database.py
@@ -1,16 +1,58 @@
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import mapped_column
 
 
+db=SQLAlchemy()
 
-
-class exampleTable(db.Model):
-    example = db.Column(db.Integer, primary_key=True)
-   
-
+##User
+class User(db.Model):
+    __tablename__ = 'user' 
+    email= db.Column(db.String, primary_key=True)
+    username = db.Column(db.String, primary_key=False, nullable=False)
+    password = db.Column(db.String, primary_key=False, nullable=False)
+    status = db.Column(db.Integer, primary_key=False, nullable=False)
+                      
     def __repr__(self):
-        return 'example table'
+        return '<userNamw %r>' % self.username
+    
+##Projects
 
+class Projects(db.Model):
+    __tablename__ = 'projects' 
+    projectName= db.Column(db.String, primary_key=True)
+    projectCode = db.Column(db.String, primary_key=False, nullable=False, unique=True)
+    piCreator = db.Column(db.String, nullable=False)  # Reference user email
+    
+    # Define the relationship correctly
+   
+                      
+    def __repr__(self):
+        return '<Project Name %r>' % self.projectName
+
+
+
+
+class Involvement(db.Model):
+    id= db.Column(db.Integer, primary_key=True,
+                index=True, autoincrement=True)
+
+    user_id = mapped_column(ForeignKey("user.email"), primary_key=True)
+    project_id = mapped_column(ForeignKey("projects.projectName"), primary_key=True)
+   
+    
+   
+    
+    userss = relationship("User", foreign_keys=[user_id])
+    projectss = relationship("Projects", foreign_keys=[project_id])
+    
+
+
+
+                      
+   
+    
 
 ##Creates the table
 def create_database(app):
