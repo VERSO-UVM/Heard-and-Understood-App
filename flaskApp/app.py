6b127c94da6e206ed653f72c3ac83dfed36cbb07
adrienmonks11 <adrien.monks@gmail.com>
2024-10-31 12:13:24 -0400

    Firebase setup

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash, json
+import firebase_admin
+from firebase_admin import credentials, firestore
+import bcrypt
+from firebase.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+
+with open('firebase/serviceAccountKey.json') as f:
+    service_account = json.load(f)
+    app.secret_key = service_account.get("secret_key")
+
+
+def initialize_firebase():
+    cred = credentials.Certificate('firebase/serviceAccountKey.json')
+    firebase_admin.initialize_app(cred)
+
+
+initialize_firebase()
+db = firestore.client()
 
 
 @app.route("/")
@@ -8,11 +27,69 @@ def home():
     return render_template('login.html')
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        hashed_password = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt())
+
+        try:
+            # Check if a user with this email already exists
+            user_ref = db.collection('users').document(email)
+            if user_ref.get().exists:
+                flash("An account with this email already exists.", "danger")
+                return redirect(url_for('register'))
+
+            user_ref.set({
+                'email': email,
+                'password': hashed_password.decode('utf-8')
+            })
+            return redirect(url_for('homepage'))
+
+        except Exception as e:
+            flash(f"An error occurred: {str(e)}", "danger")
+            return redirect(url_for('register'))
+
+
+@app.route('/forgot_password')
+def forgot_password():
+    return render_template('forgot_password.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # email = request.form.get('email')
-    # password = request.form.get('password')
-    return redirect(url_for('homepage'))
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if request.method == 'POST':
+        try:
+            # Retrieve user data from Firestore
+            user_ref = db.collection('users').document(email)
+            user_doc = user_ref.get()
+
+            if user_doc.exists:
+                stored_hashed_password = user_doc.to_dict().get('password')
+
+                # Check if the password matches
+                if bcrypt.checkpw(password.encode('utf-8'), stored_hashed_password.encode('utf-8')):
+                    return redirect(url_for('homepage'))
+                else:
+                    flash("Invalid password", "danger")
+                    return redirect(url_for('login'))
+            else:
+                flash("User not found", "danger")
+                return redirect(url_for('login'))
+
+        except Exception as e:
+            flash(f"An error occurred: {str(e)}", "danger")
+            return redirect(url_for('login'))
+    if request.method == "GET":
+        return render_template('login.html')
 
 
 @app.route("/homepage")
