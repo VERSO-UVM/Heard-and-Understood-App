1df631901a270145395a2487d147fa822950de43
aureliamarzipan <aureliakornheiser@gmail.com>
2025-02-19 15:58:17 -0500

    more credential moving

--- hua/firebase/__init__.py
+++ hua/firebase/__init__.py
@@ -1,8 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
+from hua import FIREBASE_CREDS
 
 
 def initialize_firebase():
     # Point to the service account key file inside the firebase folder
-    cred = credentials.Certificate('firebase/serviceAccountKey.json')
+    cred = credentials.Certificate(FIREBASE_CREDS)
     firebase_admin.initialize_app(cred)
